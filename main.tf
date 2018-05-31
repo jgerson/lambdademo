281d8ed22e0b51a115c7e7d22485c8455e4cdde5
jgerson <jgerson@hashicorp.com>
2018-05-31 15:41:42 -0400

    second commit

--- main.tf
+++ main.tf
@@ -6,12 +6,20 @@ provider "aws" {
   region = "us-east-1"
 }
 
+provider "archive" {}
+
+data "archive_file" "zip" {
+  type        = "zip"
+  source_file = "index.js"
+  output_path = "function.zip"
+}
+
 resource "aws_lambda_function" "demo_lambda" {
     function_name = "demo_lambda"
     handler = "index.handler"
     runtime = "nodejs4.3"
-    filename = "function.zip"
-    source_code_hash = "${base64sha256(file("function.zip"))}"
+    filename         = "${data.archive_file.zip.output_path}"
+    source_code_hash = "${data.archive_file.zip.output_sha}"
     role = "${aws_iam_role.lambda_exec_role.arn}"
 }
 
